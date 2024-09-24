59d38c0f2b9769e2f668fe0d0355e8a3e8a411ee
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-24 19:43:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    # Replace 'your-telegram-bot-token' with the actual bot token
+    BOT_TOKEN = "6610201435:AAHEv2YoM2ZEtlEdqjilv9mZGjT9Uzzrntw"
+    
+    # Tesseract OCR executable path (adjust according to your system)
+    TESSERACT_CMD = r'/usr/bin/tesseract'

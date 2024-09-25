da7576f9734f065ee54b7f65093794772514620a
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-25 12:30:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,12 @@
-# config.py
+import os
 
-class Config:
-    API_ID = "21740783"
+API_ID = "21740783"
 
-    API_HASH = "a5dc7fec8302615f5b441ec5e238cd46"
+API_HASH = "a5dc7fec8302615f5b441ec5e238cd46"
     # Replace 'your-telegram-bot-token' with the actual bot token
-    BOT_TOKEN = "6610201435:AAHEv2YoM2ZEtlEdqjilv9mZGjT9Uzzrntw"
+BOT_TOKEN = "6610201435:AAHEv2YoM2ZEtlEdqjilv9mZGjT9Uzzrntw"
      
-    # Tesseract OCR executable path (adjust according to your system)
-    TESSERACT_CMD = r'/download'
+    # Tesseract OCR executable path (adjust according to your system
+TESSERACT_CMD = r'/download'
     
-    # Start message text
-    START_MESSAGE = "Hello! I'm the Handwriting Bot. Send me a photo of your handwriting to get started!"
-    
-    # Status message text
-    STATUS_MESSAGE = "Processing your request. Please wait..."
+ 

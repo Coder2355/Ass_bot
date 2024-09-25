67684e096772ca0a5df053011c18766e27743656
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-25 07:34:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 # config.py
 
 class Config:
+    API_ID = "21740783"
+
+    API_HASH = "a5dc7fec8302615f5b441ec5e238cd46"
     # Replace 'your-telegram-bot-token' with the actual bot token
     BOT_TOKEN = "6610201435:AAHEv2YoM2ZEtlEdqjilv9mZGjT9Uzzrntw"
      
